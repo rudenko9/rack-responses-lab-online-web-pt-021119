dc57ef957844738a4a5a56d2e6ca34c9687b4db2
Nataliya R <n.rudenko@gmail.com>
2019-05-15 19:04:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,9 @@ class Application
     resp = Rack::Response.new
     time = Time.now
     if time => 12
-  resp.write "Good "
-  else 
   resp.write "Good Afternoon"
+  else 
+  resp.write "Good Morning"
 end
   resp.finish
 end 
